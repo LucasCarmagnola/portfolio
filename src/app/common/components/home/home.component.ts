59f2b17e61e69c48b14220d9edfdf30e7d0b5ca4
LucasCarmagnola <carmagnola.lucas@gmail.com>
2025-01-13 17:59:56 -0300

    cambios

--- src/app/common/components/home/home.component.ts
+++ src/app/common/components/home/home.component.ts
@@ -10,4 +10,47 @@ import { RouterModule } from '@angular/router';
 })
 export class HomeComponent {
 
+
+  ngOnInit() {
+    const leftContainer = document.querySelector('.main') as HTMLElement;
+  
+    if (leftContainer) {
+      const maxScroll = document.documentElement.scrollHeight - window.innerHeight;
+  
+      // Escucha el evento de scroll del documento
+      window.addEventListener('scroll', () => {
+        const scrollY = Math.min(window.scrollY, maxScroll); // Limita el desplazamiento máximo
+        leftContainer.style.top = `${scrollY}px`; // Mueve el contenedor izquierdo
+      });
+    }
+  }
+  
+
+  // ngOnInit() {
+  //   const leftContainer = document.querySelector('.main') as HTMLElement;
+  
+  //   if (leftContainer) {
+  //     // Obtén el alto total de la página menos el alto del contenedor izquierdo
+  //     const maxScroll = document.documentElement.scrollHeight - leftContainer.offsetHeight;
+  
+  //     // Escucha el evento de scroll en el documento
+  //     window.addEventListener('scroll', () => {
+  //       const scrollY = Math.min(window.scrollY, maxScroll); // Limita el scroll máximo
+  //       leftContainer.style.transform = `translateY(${scrollY}px)`;
+  //     });
+  //   }
+  // }
+  
+  // ngOnInit() {
+  //   const leftContainer = document.querySelector('.main') as HTMLElement;
+  
+  //   if (leftContainer) {
+  //     // Escucha el evento de scroll en el documento
+  //     window.addEventListener('scroll', () => {
+  //       // Ajusta la posición vertical del contenedor izquierdo según el scroll de la página
+  //       leftContainer.style.transform = `translateY(${window.scrollY}px)`;
+  //     });
+  //   }
+  // }
+
 }
